9f40d98b8a9f5f3491c0c995ce1025cf991fe440
manveti <manveti@users.noreply.github.com>
2017-12-12 22:22:37 -0800

    Initial layout work for D&D 3.5 XP calculator

--- XPCalc/MainWindow.xaml.cs
+++ XPCalc/MainWindow.xaml.cs
@@ -18,8 +18,25 @@ namespace XPCalc {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private TextBox elBox, partyLevelBox, partySizeBox;
+
         public MainWindow() {
             InitializeComponent();
+            this.elBox = new TextBox();
+            this.elBox.Text = "1";
+            Grid.SetRow(this.elBox, 0);
+            Grid.SetColumn(this.elBox, 1);
+            simpleGrid.Children.Add(this.elBox);
+            this.partyLevelBox = new TextBox();
+            this.partyLevelBox.Text = "1";
+            Grid.SetRow(this.partyLevelBox, 0);
+            Grid.SetColumn(this.partyLevelBox, 3);
+            simpleGrid.Children.Add(this.partyLevelBox);
+            this.partySizeBox = new TextBox();
+            this.partySizeBox.Text = "4";
+            Grid.SetRow(this.partySizeBox, 0);
+            Grid.SetColumn(this.partySizeBox, 5);
+            simpleGrid.Children.Add(this.partySizeBox);
         }
     }
 }
