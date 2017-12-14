cdca55942c2fdb3baeb0764973d6a1bfff3aa934
manveti <manveti@users.noreply.github.com>
2017-12-13 18:43:23 -0800

    Change how we report errors/warnings from XP calculations so we won't pop up so many warnings when we implement multiple monsters and multiple characters

--- XPCalc/MainWindow.xaml.cs
+++ XPCalc/MainWindow.xaml.cs
@@ -20,37 +20,40 @@ namespace XPCalc {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        enum XpError {
+            Success,
+            Failure,
+            ELTooLow,
+            ELTooHigh
+        }
+
         private SpinBox elBox, partyLevelBox, partySizeBox;
+        private XpError simpleErr = XpError.Success;
 
         public MainWindow() {
             InitializeComponent();
             this.elBox = new SpinBox();
             this.elBox.Value = 1;
             this.elBox.Minimum = 1;
-            this.elBox.KeyDown += this.simpleXpKeyDown;
+            this.elBox.ValueChanged += this.calculateSimpleXp;
             Grid.SetRow(this.elBox, 0);
             Grid.SetColumn(this.elBox, 1);
             simpleGrid.Children.Add(this.elBox);
             this.partyLevelBox = new SpinBox();
             this.partyLevelBox.Value = 1;
             this.partyLevelBox.Minimum = 1;
-            this.partyLevelBox.KeyDown += this.simpleXpKeyDown;
+            this.partyLevelBox.ValueChanged += this.calculateSimpleXp;
             Grid.SetRow(this.partyLevelBox, 0);
             Grid.SetColumn(this.partyLevelBox, 3);
             simpleGrid.Children.Add(this.partyLevelBox);
             this.partySizeBox = new SpinBox();
             this.partySizeBox.Value = 4;
             this.partySizeBox.Minimum = 1;
-            this.partySizeBox.KeyDown += this.simpleXpKeyDown;
+            this.partySizeBox.ValueChanged += this.calculateSimpleXp;
             Grid.SetRow(this.partySizeBox, 0);
             Grid.SetColumn(this.partySizeBox, 5);
             simpleGrid.Children.Add(this.partySizeBox);
-        }
-
-        public void simpleXpKeyDown(object sender, KeyEventArgs e) {
-            if (e.Key == Key.Return) {
-                this.calculateSimpleXp(sender, e);
-            }
+            this.calculateSimpleXp(null, null);
         }
 
         public void calculateSimpleXp(object sender, RoutedEventArgs e) {
@@ -79,11 +82,28 @@ namespace XPCalc {
                 MessageBox.Show("Party size must be at least 1", "Error");
                 return;
             }
-            xpBox.Text = "" + this.calculateXp(el, level, count);
+            xpBox.Text = "" + this.calculateXp(el, level, count, out this.simpleErr);
+            this.simpleNotesBut.IsEnabled = (this.simpleErr != XpError.Success);
         }
 
-        private int calculateXp(int cr, int level, int count, String opponent = "Encounter", String character = "party") {
+        private void getSimpleNotes(object sender, RoutedEventArgs e) {
+            String err = "An error occurred", severity = "Error";
+            switch (this.simpleErr) {
+            case XpError.ELTooLow:
+                err = "Encounter is more than 7 levels below party; no XP will be awarded";
+                severity = "Notice";
+                break;
+            case XpError.ELTooHigh:
+                err = "Encounter is more than 7 levels above party; consider alternate XP award";
+                severity = "Warning";
+                break;
+            }
+            MessageBox.Show(err, severity);
+        }
+
+        private int calculateXp(int cr, int level, int count, out XpError err) {
             double xp = 0;
+            err = XpError.Success;
             if (level <= 3) {
                 level = 3;
             }
@@ -92,11 +112,11 @@ namespace XPCalc {
             }
             int diff = cr - level;
             if (diff < -7) {
-                MessageBox.Show(opponent + " is more than 7 levels below " + character + "; no XP will be awarded", "Warning");
+                err = XpError.ELTooLow;
                 return 0;
             }
             if (diff > 7) {
-                MessageBox.Show(opponent + " is more than 7 levels above " + character + "; consider alternate XP award", "Warning");
+                err = XpError.ELTooHigh;
             }
             for (xp = 300; diff < -1; diff += 2) {
                 xp /= 2;
